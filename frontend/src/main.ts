b2bbba9c7979fec4ad215cd50fe30bc62e8d760a
maarten <19326293+afx42@users.noreply.github.com>
2025-02-07 16:12:53 +0100

    added RUM

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -10,3 +10,20 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+    applicationId: '090ce9b7-6e51-491b-ae52-c674e9591e07',
+    clientToken: 'pub3f4cc2cc2fbbad9addf9d7194c9eb0b5',
+    // `site` refers to the Datadog site parameter of your organization
+    // see https://docs.datadoghq.com/getting_started/site/
+    site: 'datadoghq.com',
+    service: 'cloudtodoapp',
+    env: 'prod',
+    // Specify a version number to identify the deployed version of your application in Datadog
+    // version: '1.0.0',
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 20,
+    defaultPrivacyLevel: 'allow',
+});
